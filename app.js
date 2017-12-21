36100bdaef0e65e707101702d9d50923c4932a74
cem dırman <cemdrman@gmail.com>
2017-12-21 03:16:20 +0300

    added search function

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ app.get("/",function(req,res){
   res.render("search");//search adlı sayfaya yönlendirme yapıyor
 });
 
+app.get("/results",function(req,res){
+  var query = req.query.search; //search.ejs içerisindeki input name yani film adı gelecek
+  var url = "https://api.themoviedb.org/3/search/movie?api_key=1b85f98471960da3a8159b93bf91d47e&quer=" + query;
+  request(url,function(error,resonce,body){
+    if (resonce.statusCode == 200) {
+      var data = JSON.parse(body);
+      console.log(body);
+      res.render("results",{data:data});
+    }else{
+      res.render("404"); //hata alırsak
+    }
+  });
+
+
+
+});
 
 
 var server = app.listen(3000,function(){
